3f7f05c3a0ef994521afdd170f90c927baef2f67
FranMendo2002 <franmendozita123@gmail.com>
2021-08-22 00:20:50 -0300

    Cambio index.php y crear.php

--- admin/index.php
+++ admin/index.php
@@ -7,8 +7,6 @@ incluirTemplate('header');
     <h1>Administrador de Bienes Raices</h1>
 
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
-    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
-    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 </main>
 
 <?php incluirTemplate('footer'); ?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,49 @@ incluirTemplate('header');
     <h1>Crear</h1>
 
     <a href="/admin" class="boton boton-verde">Volver</a>
+
+    <form class="formulario">
+        <fieldset>
+            <legend>Información General</legend>
+
+            <label for="titulo">Título:</label>
+            <input type="text" id="titulo" placeholder="Título de la propiedad">
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" placeholder="Precio de la propiedad">
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="imagen">Descripcion:</label>
+            <textarea id="descripcion"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="estacionamiento">Estacionamiento:</label>
+            <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="" id="">
+                <option value="" selected disabled>-- Seleccione un vendedor --</option>
+                <option value="1">Juan</option>
+                <option value="1">Franco</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
 </main>
 
 <?php incluirTemplate('footer'); ?>
