86e83a0027bb3f74c6869d57b15a444280dcad67
FranMendo2002 <franmendozita123@gmail.com>
2021-09-02 19:31:26 -0300

    Crud terminado

--- anuncio.php
+++ anuncio.php
@@ -1,36 +1,58 @@
 <?php
     require 'includes/funciones.php';
+    require 'includes/config/database.php';
     incluirTemplate('header');
+
+    // Conectar DB
+    $db = conectarDB();
+    
+    // Obtenemos el id
+    $id = $_GET["id"] ?? null;
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if($id) {
+        $query = "SELECT * FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db, $query);
+        if($resultado->num_rows === 0) {
+            header('Location: /');
+        }
+    } else {
+        header('Location: /');
+    }
 ?>
 
-<main class="contenedor seccion contenido-centrado">
-    <h1>Casa en Venta frente al bosque</h1>
+<?php 
+    while($propiedad = mysqli_fetch_assoc($resultado)):
+?>
+    <main class="contenedor seccion contenido-centrado">
+            <h1> <?php echo $propiedad["titulo"]; ?> </h1>
 
-    <picture>
-        <source srcset="build/img/destacada.webp" type="image/webp">
-        <source srcset="build/img/destacada.jpg" type="image/jpeg">
-        <img loading="lazy" src="build/img/destacada.jpg" alt="Imagen de la propiedad">
-    </picture>
+            <img loading="lazy" src="imagenes/<?php echo $propiedad["imagen"]; ?>" alt="Imagen de la propiedad">
 
-    <div class="resumen-propiedad">
-        <p class="precio">$3,000,000</p>
-        <ul class="iconos-caracteristicas">
-            <li>
-                <img loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                <p>3</p>
-            </li>
-            <li>
-                <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                <p>3</p>
-            </li>
-            <li>
-                <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                <p>4</p>
-            </li>
-        </ul>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quod sunt quasi consectetur commodi explicabo, tenetur rerum, blanditiis ratione laboriosam aut odio id accusamus ab magni perferendis nam eos libero. Ipsam doloremque tenetur labore quidem exercitationem rem, laborum totam natus nostrum optio non earum impedit asperiores alias debitis. Quis facilis nemo labore omnis tenetur sed error nulla ea dolor dolorum!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias aliquid voluptatibus magni laudantium reiciendis molestias maxime delectus, porro sapiente eum ratione commodi ut unde fuga officia dolore illum deserunt possimus.</p>
-    </div>
-</main>
+            <div class="resumen-propiedad">
+                <p class="precio">$<?php echo $propiedad["precio"]; ?></p>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                        <p><?php echo $propiedad["wc"]; ?></p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                        <p><?php echo $propiedad["estacionamiento"]; ?></p>
+                    </li>
+                    <li>
+                        <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                        <p><?php echo $propiedad["habitaciones"]; ?></p>
+                    </li>
+                </ul>
+                <p><?php echo $propiedad["descripcion"]; ?></p>
+            </div>
+    </main>
 
-<?php incluirTemplate('footer'); ?>
+<?php 
+    endwhile;
+    incluirTemplate('footer'); 
+
+    // Cerrar la conexion
+    mysqli_close($db);
+?>
